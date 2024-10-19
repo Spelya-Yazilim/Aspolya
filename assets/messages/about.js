1e2fcdc09aead90b6bfdfc70e2acf829bec7c59a
Prestgg1 <prestgg12@gmail.com>
2024-10-19 18:12:47 +0400

    Hakkımızda

--- assets/messages/about.js
+++ assets/messages/about.js
@@ -2,7 +2,64 @@ import localization from "../js/localization.js";
 import { staticData } from "./static.js";
 const messages = {
   en: {
-   ...staticData.en,
+    ...staticData.en,
+    hero_title: `Working to create a cleaner, greener world`,
+    hero_image: "Image from Unsplash",
+    contact: "Contact Us",
+    service1: "DESIGN-BUILD",
+    service2: "EV YENİLEME",
+    service3: "CITY SPACES",
+    service4: "CIVIL ENGINEERS",
+    aboutContext: "Our businesses stretch across construction and regeneration in the built environment.",
+    aboutUs: "About Us",
+    aboutContent: `Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            qui officia deserunt mollit anim id est laborum.`,
+    aboutContent2: `Oh feel if up to till like. He an thing rapid these after going drawn or. Timed she his law
+            the spoil round defer. In surprise concerns informed betrayed he learning is ye. Ignorant formerly so ye
+            blessing.`,
+    aboutContentParagraph: `He as spoke avoid given downs money on we. Of properly carriage shutters ye as wandered up repeated
+            moreover.`,
+    aboutContentService1: `  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum.
+    `,
+    aboutContentService2: `  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum.
+    `,
+    aboutContentService3: `  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum.
+    `,
+    galerySection: `Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Ipsam, perferendis adipisci ad ratione quam quibusdam`,
+
+    aboutTeamTitle: `Our Team About`,
+
+    aboutTeamContent: `Every project is undertaken with innovation, quality and customer expectations at the forefront of our collective minds. Continuous improvement is embedded within our company culture and we strive to exceed customer expectations in quality and response time. We are proud to present our achievements in three major markets.
+    `,
+    aboutCompany: "About Company",
+    aboutCompanyContent: `Oh feel if up to till like. He an thing rapid these after going drawn or.
+            Timed she his law the spoil round defer. In surprise concerns informed betrayed he learning is ye. Ignorant
+            formerly so ye blessing.`,
+    aboutCompanyContentParagraph: `He as spoke avoid given downs money on we. Of properly carriage
+            shutters ye as wandered up repeated moreover. `,
+    aboutCompanyContent1:`Placerat orci nulla pellentesque dignissim enim sit. Cursus turpis massa tincidunt dui ut
+              ornare lectus. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Accumsan sit
+              amet nulla facilisi morbi tempus iaculis urna. Neque egestas congue quisque egestas diam in arcu.`,
+    popupTitle:`Full-Service Engineering`,
+    popupContent:`Sample text. Click to select the text box. Click again or double click
+              to start editing the text.`,
+    popupButton:`Learn More`,
+    submit:"Submit",
+    callus:"Call Us",
+    ourhours:"Our Hours",
+
+
+
+
+
   }
 }
 localization(messages)
